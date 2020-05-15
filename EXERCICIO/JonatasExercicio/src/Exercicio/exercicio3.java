702e9cd8164500d06b497f3712db980f353101af
jonatasdamascenoalves <61021433+jonatasdamascenoalves@users.noreply.github.com>
2020-05-15 13:32:02 -0300

    exercicio

---
+++ EXERCICIO/JonatasExercicio/src/Exercicio/exercicio3.java
@@ -0,0 +1,13 @@
+package Exercicio;
+//3- Faça um programa que mostre uma contagem na tela de 233 a 456, só que contando de
+//3 em 3 quando estiver entre 300 e 400 e de 5 em 5 quando não estiver.
+
+public class exercicio3 {
+	public static void main(String[] args) {
+		for (int i = 233; i < 456; i+=3) {
+			if (i >299 && i < 401) {
+						i+=5;
+			}
+			System.out	.println(i);	}
+	}
+}
